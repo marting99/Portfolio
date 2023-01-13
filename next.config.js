911ac540af45b0b7e13bdc77cc14ea219848e4c7
Martin Garcia <martingarcia9931@gmail.com>
2023-01-13 14:03:28 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env:{
-    NEXT_PUBLIC_ENV:'service_oqv67mk',
-    NEXT_PUBLIC_EMAILJS_PUBLIC_KEY:"Kifhx_WL59Vt1N7K6",
-    NEXT_PUBLIC_EMAILJS_TEMPLATE_ID:"template_e2l1414",
+    
   }
 }
 
