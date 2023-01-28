0e95e7a91bea47909dc7e10629c4a1054fdfdb7b
marting99 <martingarcia9931@gmail.com>
2023-01-27 19:49:55 -0800

    add styled-components and create global style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,28 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
-const inter = Inter({ subsets: ['latin'] })
-
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: 'Playfair Display', serif;
+    // background-color: #686D77;
+    background-color:#68776B;
+    margin: 0;
+    color: white;
+    padding: 0;
+  }
+`
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Martin Garcia</title>
+        <title>Martin Garcia Dev</title>
       </Head>
       <main >
+      <GlobalStyles />
         <div>
-          <p>home</p>
+          <h1>Martin Garcia</h1>
         </div>
       </main>
     </>
