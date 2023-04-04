775a9d19078b1399ef2788b396bbb74a3994d557
marting99 <martingarcia9931@gmail.com>
2023-04-04 13:42:26 -0700

    add nav-style

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,6 +1,6 @@
 import React,{useEffect, useState} from 'react';
-import styled from 'styled-components'
 import {AiOutlineMenu,AiOutlineClose} from 'react-icons/ai';
+import {MobileNav,Navbar} from './nav-style'
 
 type props = {
     pageName?:string;
@@ -8,90 +8,7 @@ type props = {
     open?:boolean;
     setOpen?:React.Dispatch<React.SetStateAction<boolean>>
 }
-const Navbar = styled.div`
-    display:flex;
-    top:0;
-    position:sticky;
-    height:60px;
-    z-index:10;
-    justify-content:space-between;
-    align-items:center;
-    color:black;
-    background-color:white;
-    box-shadow: 0px 7px 13px -1px #111;
-    padding:0 20px;
-    h1{
-        margin:0;
-    }
-    .content{
-        display:flex;
-        justify-content:center;
-        font-size:24px;
-    }
-    .content .close{
-        display:none;
-    }
-    .content .open{
-        display:none;
-    }
-    .content a{
-        margin:0px 10px;
-    }
-    .content a:hover{
-        cursor:pointer;
-    }
-    @media(max-width:900px){
-        display:none;
-        button{
-            display:none;
-        }
-    }
-`;
-const MobileNav = styled.div`
-display:none;
-    @media(max-width:900px){
-        display:flex;
-        .hide{
-            transform: translateX(-100%);
-            transition:all .5s ease-out;
-        }
-        ul{
-            margin:0;
-            padding:0;
-            display:flex;
-            flex-wrap:wrap;
-            flex-direction:column;
-            align-items:center;
-            list-style:none;
-            position:fixed;
-            background-color:black;
-            height:100%;
-            width:80%;
-            overflow-x:hidden;
-            z-index:1;
-            top:0;
-            left:0;
-            position:absolute;
-            
-            right:25px;
-            top:0;
-        }
-        .show{
-            transform: translateX(-3%);
-            transition:all .7s ease-out;
-        }
-        ul .link{
-            margin:40px 0px;
-        }
-        button{
-            display:flex;
-            color:white;
-            background-color: #68776B;
-            border:none;
-            font-size:32px;
-        }
-    }
-`;
+
 export const Nav:React.FC<props>=({handleClick,setOpen,open})=>{
     const [isOpen,setIsOpen]=useState(false);
     return(
